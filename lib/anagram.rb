3b923debd6bfaaa40af662aa368d9cb66e00d06e
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-02 18:23:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |array_word|
+      self.word.split("").sort
+  end
+end
